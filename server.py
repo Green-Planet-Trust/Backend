5c2b0a1ced44019185688b283c7ddcfad7fb8061
jse41 <54693948+jse41@users.noreply.github.com>
2020-07-15 19:26:39 -0400

    Working model of planned data structure

--- server.py
+++ server.py
@@ -41,28 +41,22 @@ product_ns = api.namespace(
 # Define the API models we will use (these will show up in the Swagger Specification).
 
 rating = api.model('Rating', {
-    'efficiency': fields.Integer(required=False, description='The efficiency-in-use rating (0-9, where 0 is best) of this item'),
-    'energy': fields.Float(required=False, description='The energy (J) to produce this item'),
-    'CO2': fields.Float(required=False, description='The CO2 released (Kg) to produce this item'),
-    'otherGG': fields.Float(required=False, description='The other green house gases released (Kg) to produce this item'),
-    'water': fields.Float(required=False, description='The volume of water (litres) to produce this item'),
-    'plastic': fields.Float(required=False, description='The amout of plastic (Kg) included in this item'),
-    'lifetime': fields.Float(required=False, description='The expected lifetime (years) of this item'),
-    'recyclability': fields.Integer(required=False, description='The recyclability rating (0-9, where 0 is best) of this item'),
-    'repairability': fields.Integer(required=False, description='The Right to Repair rating (0-9, where 0 is best) of this item')
+    'Production': fields.Float(required=False, description='The efficiency-in-use rating (0-9, where 0 is best) of this item'),
+    'Transportation': fields.Float(required=False, description='The energy (J) to produce this item'),
+    'Retail': fields.Float(required=False, description='The CO2 released (Kg) to produce this item'),
 })
 
 product = api.model('Product', {
     'id': fields.String(readonly=True, description='The unique product registration identifier'),
-    'barcode_id': fields.String(required=True, description='The barcode for this product id, in EAN-13 format'),
-    'type': fields.String(required=True, description='The type of product'),
-    'category': fields.String(required=True, description='The category of this product, with its type'),
-    'model': fields.String(required=True, description='The model number of this product'),
-    'brand': fields.String(required=True, description='The venfor of this item'),
-    'rating_data': fields.Nested(rating)
+    'UID': fields.String(readonly=True, description='The unique product registration identifier'),
+    'Carbon-Footprint': fields.Float(required=True, description='The barcode for this product id, in EAN-13 format'),
+    'Current-Company': fields.String(required=True, description='The type of product'),
+    'isRecycleable': fields.String(required=True, description='The category of this product, with its type'),
+    'Stages': fields.Nested(rating),
+    'Date': fields.String(required=True, description='The category of this product, with its type')
 })
 
-db_name = 'cir-db'
+db_name = 'cir-db2'
 
 # A Data Access Object to handle the reading and writing of Product records to the Cloudant DB
 
@@ -78,28 +72,22 @@ class ProductDAO(object):
 
     def import_data(self):
         print("Importing dummy data", end='', flush=True)
-        with open('dummy-data.txt') as csv_file:
+        with open('newDummyData.txt') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             line_count = 0
             for row in csv_reader:
                 if line_count > 0:
                     data = {
-                        'barcode_id': row[4],
-                        'type': row[0],
-                        'category': row[2],
-                        'model': row[3],
-                        'brand': row[1],
-                        'rating_data': {
-                            'efficiency': int(row[6]),
-                            'energy': float(row[7]) + float(row[8]),
-                            'CO2': float(row[13]),
-                            'otherGG': float(row[14]),
-                            'water': float(row[11]),
-                            'plastic': float(row[9]),
-                            'lifetime': float(row[10]),
-                            'recyclability': int(row[12]),
-                            'repairability': int(row[15])
-                        }
+                        'UID': row[0],
+                        'Carbon-Footprint': float(row[1]),
+                        'Current-Company': row[2],
+                        'isRecycleable': row[3],
+                        'Stages': {
+                            'Production': float(row[4]),
+                            'Transportation': float(row[5]),
+                            'Retail': float(row[6])
+                        },
+                        'Date': row[7]
                     }
                     # Have to rate limit it to less than 10 a second, due to free tier
                     time.sleep(0.15)
@@ -114,7 +102,7 @@ class ProductDAO(object):
     def get(self, id):
         try:
             my_document = self.cir_db[id]
-            my_document['id'] = my_document['barcode_id']
+            my_document['id'] = my_document['UID']
         except KeyError:
             api.abort(404, "Product {} not registered".format(id))
         return my_document
@@ -133,9 +121,9 @@ class ProductDAO(object):
         # For now, we'll set the id to be the same as the barcode_id. For production systems, we would
         # probably want these seperate, and to implement indexed searching by barcode_id for GET.
         try:
-            data['_id'] = data['barcode_id']
+            data['_id'] = data['UID']
             my_document = self.cir_db.create_document(data)
-            my_document['id'] = my_document['barcode_id']
+            my_document['id'] = my_document['UID']
         except KeyError:
             api.abort(404, "Product {} already registered".format(id))
         return my_document
